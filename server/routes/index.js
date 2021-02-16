e808a7b047a7f12533f98780ddbd2ab1e4220910
valeriadmitrievna1506dev <valeriakluch.dev@gmail.com>
2021-02-16 16:40:50 +0300

    create app.post with todoItemsController

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
+const todoItemController = require('../controllers').TodoItems;
+
 module.exports = (app) => {
   app.get('/', (req, res) =>
     res.status(200).send({
-      message: 'Welcome to the Todos API. Its start of final project',
+      message: 'Welcome to the Todo-APP',
     })
-  );
+  )
+
+  app.post('/items', todoItemController.create)
 };
