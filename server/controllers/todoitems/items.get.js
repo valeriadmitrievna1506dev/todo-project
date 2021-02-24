b51003cbb6028df576f9cd18cd78494e280bafb5
valeriadmitrievna1506dev <valeriakluch.dev@gmail.com>
2021-02-24 17:01:52 +0300

    refactor get items function

--- server/controllers/todoitems/items.get.js
+++ server/controllers/todoitems/items.get.js
@@ -1,87 +1,28 @@
 const TodoItem = require('../../models').TodoItem;
 
+const filtersToPostgres = {
+  'normal': [['createdAt']],
+  'reverse': [['createdAt', 'DESC']],
+  'done': {done: true},
+  'undone': {done: false}
+}
+
 module.exports = {
   list: async (req, res) => {
-    if (req.query.done === 'all') return listAll(req, res);
-    return listVariative(req, res);
+    const filters = {
+      order: filtersToPostgres[req.query.order],
+      done: filtersToPostgres[req.query.done]
+    }
+    console.log(filters);
+    try {
+      const result = await TodoItem.findAll({
+        order: filters.order,
+        where: filters.done
+      })
+      res.status(200).send(result)
+    } catch (error) {
+      res.status(400).send(error.message)
+    }
   },
 };
 
-const listAll = async (req, res) => {
-  try {
-    let reqOrder;
-    switch (req.query.order) {
-      case 'normal': {
-        reqOrder = [['createdAt']];
-        break;
-      }
-      case 'reverse': {
-        reqOrder = [['createdAt', 'DESC']];
-        break;
-      }
-      default:
-        return res.status(400).send({
-          message: 'Not Found Date Order Param',
-        });
-    }
-    const todoItems = await TodoItem.findAll({
-      raw: true,
-      order: reqOrder,
-    });
-    res.status(200).send(todoItems);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-};
-
-const listVariative = async (req, res) => {
-  try {
-    let reqOrder;
-    switch (req.query.order) {
-      case 'normal': {
-        reqOrder = [['createdAt']];
-        break;
-      }
-      case 'reverse': {
-        reqOrder = [['createdAt', 'DESC']];
-        break;
-      }
-      case undefined: {
-        return res.status(400).send({
-          message: 'Not Found Date Order Param',
-        });
-        break;
-      }
-      default:
-        break;
-    }
-    let doneParam;
-    switch (req.query.done) {
-      case 'done': {
-        doneParam = { done: true };
-        break;
-      }
-      case 'undone': {
-        doneParam = { done: false };
-        break;
-      }
-      case undefined: {
-        return res.status(400).send({
-          message: 'Not Found Completeness Parameter',
-        });
-        break;
-      }
-      default:
-        break;
-    }
-    const todoItems = await TodoItem.findAll({
-      raw: true,
-      order: reqOrder,
-      where: doneParam,
-    });
-    res.status(200).send(todoItems);
-  } catch (err) {
-    console.log(err.message);
-    res.status(400).send(err.message);
-  }
-};
