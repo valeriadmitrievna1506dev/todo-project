38c75add21c67419123d4784a4dd9f56a876e95b
valeriadmitrievna1506dev <valeriakluch.dev@gmail.com>
2021-02-24 18:06:42 +0300

    route get one

--- server/controllers/todoitems/item.get.js
+++ server/controllers/todoitems/item.get.js
@@ -1,17 +1,19 @@
 const TodoItem = require('../../models').TodoItem;
+const express = require('express');
+const router = express.Router();
 
-module.exports = {
-  retrieve: async (req, res) => {
-    try {
-      const todoItem = await TodoItem.findByPk(req.params.id);
-      if (!todoItem) {
-        return res.status(404).send({
-          message: 'Item Not Found',
-        });
-      }
-      res.status(200).send(todoItem);
-    } catch (err) {
-      res.status(400).send(err);
+router.get('/:id', async (req, res) => {
+  try {
+    const todoItem = await TodoItem.findByPk(req.params.id);
+    if (!todoItem) {
+      return res.status(404).send({
+        message: 'Item Not Found',
+      });
     }
-  },
-};
+    res.status(200).send(todoItem);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+module.exports = router;
